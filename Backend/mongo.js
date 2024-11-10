252639b94e115dc60ca7506b0ccbd9b492b28d4f
Vishwa Badrinadh <86405291+Vishwa172107@users.noreply.github.com>
2024-11-10 17:55:43 +0530

    Update mongo.js

--- Backend/mongo.js
+++ Backend/mongo.js
@@ -15,18 +15,18 @@ const my_Schema = mongoose.Schema({
 
 const my_model = mongoose.model("doctors_collection", my_Schema)
 
-// const doctors = [
-//     { name: 'Dr. John Doe', photo: '/d1.jpg', speciality: 'Cardiologist', rating: 4.8 },
-//     { name: 'Dr. Jane Smith', photo: '/d2.jpg', speciality: 'Dermatologist', rating: 4.6 },
-//     { name: 'Dr. Sarah Lee', photo: '/d3.jpg', speciality: 'Pediatrician', rating: 4.7 },
-//     { name: 'Dr. Mike Johnson', photo: '/d4.jpg', speciality: 'Neurologist', rating: 4.9 },
-//     { name: 'Dr. Ben 10', photo: '/d5.jpg', speciality: 'Physician', rating: 5.0 },
-//     { name: 'Dr. Iron man', photo: '/d7.jpg', speciality: 'Nephrologist', rating: 4.5 },
-//     { name: 'Dr. Natasha', photo: '/d6.jpg', speciality: 'Dentist', rating: 4.7 },
-//     { name: 'Dr. Tony Tony Chopper', photo: '/d8.jpg', speciality: 'Physician', rating: 4.4 },
-//   ];
+const doctors = [
+    { name: 'Dr. John Doe', photo: '/d1.jpg', speciality: 'Cardiologist', rating: 4.8 },
+    { name: 'Dr. Jane Smith', photo: '/d2.jpg', speciality: 'Dermatologist', rating: 4.6 },
+    { name: 'Dr. Sarah Lee', photo: '/d3.jpg', speciality: 'Pediatrician', rating: 4.7 },
+    { name: 'Dr. Mike Johnson', photo: '/d4.jpg', speciality: 'Neurologist', rating: 4.9 },
+    { name: 'Dr. Ben 10', photo: '/d5.jpg', speciality: 'Physician', rating: 5.0 },
+    { name: 'Dr. Iron man', photo: '/d7.jpg', speciality: 'Nephrologist', rating: 4.5 },
+    { name: 'Dr. Natasha', photo: '/d6.jpg', speciality: 'Dentist', rating: 4.7 },
+    { name: 'Dr. Tony Tony Chopper', photo: '/d8.jpg', speciality: 'Physician', rating: 4.4 },
+  ];
 
-//   my_model.insertMany(doctors)
+  my_model.insertMany(doctors)
 
 const appointment_schema = mongoose.Schema({
     patientName:String,
